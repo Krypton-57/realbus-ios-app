182cd2edec4d9f33d7cd887fa2ad62d917986b37
krypton-57 <contact@matheobollini.fr>
2024-06-07 21:28:33 +0200

    first commit

---
+++ RealBus/RealBus/RealBusApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealBusApp.swift
+//  RealBus
+//
+//  Created by matheo on 07/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct RealBusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
